89d8f596bafd13f974213c6c42a9870ee0a7930e
SegaSonic-88 <113409191+SegaSonic-88@users.noreply.github.com>
2022-09-13 09:10:18 -0600

    Added Truth_Or_Dare mechanic as well as direction/arrow mechanic

--- main.ts
+++ main.ts
@@ -1,3 +1,81 @@
-basic.forever(function () {
-	
+let Fate = 0
+let Direction2 = 0
+input.onButtonPressed(Button.A, function () {
+    Fate = randint(0, 1)
+    if (Fate == 0) {
+        basic.showString("Truth")
+    } else {
+        basic.showString("DARE!")
+    }
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    Direction2 = randint(0, 6)
+    if (Direction2 == 0) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (Direction2 == 1) {
+        basic.showLeds(`
+            . # # # #
+            . . . # #
+            . . # . #
+            . # . . #
+            # . . . .
+            `)
+    } else if (Direction2 == 2) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # . #
+            . . . # .
+            . . # . .
+            `)
+    } else if (Direction2 == 3) {
+        basic.showLeds(`
+            # . . . .
+            . # . . #
+            . . # . #
+            . . . # #
+            . # # # #
+            `)
+    } else if (Direction2 == 4) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # . # .
+            . . # . .
+            `)
+    } else if (Direction2 == 5) {
+        basic.showLeds(`
+            . . . . #
+            # . . # .
+            # . # . .
+            # # . . .
+            # # # # .
+            `)
+    } else if (Direction2 == 6) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # . # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (Direction2 == 6) {
+        basic.showLeds(`
+            # # # # .
+            # # . . .
+            # . # . .
+            # . . # .
+            . . . . #
+            `)
+    }
+    basic.pause(2000)
+    basic.clearScreen()
 })
